73dbc62a61c0a507638ffa1aedc897b367a75848
weiwongjs <weiwongjava@gmail.com>
2020-11-27 11:44:50 +0800

    nuxt config js change and ico photo replace

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,13 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hbw',
+    title: '华邦教育',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'keywords', name: 'keywords', content: '华邦教育' },
+      { hid: 'description', name: 'description', content: '华邦教育' }
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
